02744fd567c2cfcd19f311ef64e55da45a35f243
reinhardtc <reinhardtc22@gmail.com>
2021-05-31 15:26:44 -0500

    connected to Express server

--- utlis/inputCheck.js
+++ utlis/inputCheck.js
@@ -1,10 +1,10 @@
 // utility to check if an object has the required properties
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
 
-module.exports = function(obj, ...props) {
+module.exports = function (obj, ...props) {
   const errors = [];
 
-  props.forEach((prop) => {
+  props.forEach(prop => {
     // if property is blank or doesn't exist, add to errors array
     if (obj[prop] === undefined || obj[prop] === '') {
       errors.push(`No ${prop} specified.`);
@@ -13,9 +13,9 @@ module.exports = function(obj, ...props) {
 
   if (errors.length) {
     return {
-      error: errors.join(' ')
+      error: errors.join(' '),
     };
   }
-  
+
   return null;
 };
